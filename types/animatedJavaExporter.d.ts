8c0076cad4131631191dc7118dac903f69154a39
SnaveSutit <snavesutit@gmail.com>
2023-04-10 09:12:46 -0400

    Update to AJ 0.3.6

--- types/animatedJavaExporter.d.ts
+++ types/animatedJavaExporter.d.ts
@@ -4,23 +4,24 @@ declare global {
 	namespace AnimatedJava {
 		type NamespacedString = `${string}${string}:${string}${string}`
 		type ProjectSettings = Record<NamespacedString, Setting<any>>
+		export interface IAnimatedJavaExportData<S> {
+			ajSettings: IAnimatedJavaSettings
+			projectSettings: AnimatedJava.IProjectSettings
+			exporterSettings: S
+			renderedAnimations: AnimatedJava.IRenderedAnimation[]
+			rig: AnimatedJava.IRenderedRig
+		}
 	}
 }
 
-interface IAnimatedJavaExporterOptions<S extends AnimatedJava.ProjectSettings> {
+export interface IAnimatedJavaExporterOptions<S extends AnimatedJava.ProjectSettings> {
 	id: AnimatedJava.NamespacedString
 	name: string
 	description: string
 	getSettings(): S
 	settingsStructure: AnimatedJava.GUIStructure
 	onStartup?: () => void
-	export(
-		ajSettings: IAnimatedJavaSettings,
-		projectSettings: AnimatedJava.IProjectSettings,
-		exporterSettings: S,
-		renderedAnimations: AnimatedJava.IRenderedAnimation[],
-		rig: AnimatedJava.IRenderedRig
-	): Promise<void> | void
+	export(exportData: AnimatedJava.IAnimatedJavaExportData<S>): Promise<void> | void
 }
 
 export class AnimatedJavaExporter<
